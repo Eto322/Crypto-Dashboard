04f6922df49d5debcdd0d40ab60b44c8683728fa
Eto322 <etoakame322@yahoo.com>
2024-10-04 17:02:52 +0300

    Implement CredentialManager class for managing API keys; add CoinGecko and CoinCap keys in appsettings.json

---
+++ DAL/Credentials/CredentialManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace DAL.Credentials
+{
+    public class CredentialManager
+    {
+        private readonly string _geckoApiKey;
+        private readonly string _coincapApiKey;
+        public CredentialManager()
+        {
+            // Get the path to the project's root directory
+            string basePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName;
+
+            // Create a configuration builder and specify the path to the configuration file
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(basePath) 
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true); 
+
+            // Build the configuration from the specified sources
+            IConfigurationRoot configuration = builder.Build();
+
+            // Retrieve the CoinGecko API key from the configuration settings
+            _geckoApiKey = configuration["ApiSettings:CoinGeckoApiKey"];
+            _coincapApiKey = configuration["ApiSettings:CoinCapApiKey"];
+        }
+
+        public string GetGeckoApiKey()
+        {
+            return _geckoApiKey;
+        }
+
+        public string GetCoinCapApiKey()
+        {
+            return _coincapApiKey;
+        }
+    }
+}
