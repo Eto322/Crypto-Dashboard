b2f83fd4144c69e10ee98ec7a0a95802e3d580b3
Eto322 <etoakame322@yahoo.com>
2024-10-06 15:26:03 +0300

    Rewrite merger #Due to a bug that some coins can have identical symbols, causing a crash due to an existing key in the dictionary.

--- BLL/Manager/Helper/CryptocurrencyMerger.cs
+++ BLL/Manager/Helper/CryptocurrencyMerger.cs
@@ -9,57 +9,83 @@ namespace BLL.Manager.Helper
 {
     public class CryptocurrencyMerger
     {
-        public List<Cryptocurrency> MergeCryptocurrencyData(List<Cryptocurrency> capData, List<Cryptocurrency> geckoData)
+        public List<Cryptocurrency> MergeCryptocurrencyData(List<Cryptocurrency> capData,
+            List<Cryptocurrency> geckoData)
         {
             var mergedData = new List<Cryptocurrency>();
 
-            //  Dictionary for quick lookup of CoinGecko data by IdGecko
-            var geckoDictionary = geckoData.ToDictionary(c => c.Symbol.ToUpper());
+           
 
-            // Iterate over CoinCap data and merge
-            foreach (var capCrypto in capData)
+
+            if (capData != null && capData.Any())
             {
-                // Cryptocurrency object to store merged data
-                var mergedCrypto = new Cryptocurrency
+                // Iterate over CoinCap data and merge
+                foreach (var capCrypto in capData)
                 {
-                    IdCap = capCrypto.IdCap,
-                    Symbol = capCrypto.Symbol,
-                    Name = capCrypto.Name,
-                    CirculatingSupply = capCrypto.CirculatingSupply,
-                    MaxSupply = capCrypto.MaxSupply,
-                    MarketCapUsd = capCrypto.MarketCapUsd,
-                    VolumeUsd24Hr = capCrypto.VolumeUsd24Hr,
-                    CurrentPrice = capCrypto.CurrentPrice,
-                    ChangePercent24Hr = capCrypto.ChangePercent24Hr,
-                    Explorer = capCrypto.Explorer,
-                    MarketCapRank = capCrypto.MarketCapRank,
-                };
+                    var mergedCrypto = new Cryptocurrency
+                    {
+                        IdCap = capCrypto.IdCap,
+                        Symbol = capCrypto.Symbol,
+                        Name = capCrypto.Name,
+                        CirculatingSupply = capCrypto.CirculatingSupply,
+                        MaxSupply = capCrypto.MaxSupply,
+                        MarketCapUsd = capCrypto.MarketCapUsd,
+                        VolumeUsd24Hr = capCrypto.VolumeUsd24Hr,
+                        CurrentPrice = capCrypto.CurrentPrice,
+                        ChangePercent24Hr = capCrypto.ChangePercent24Hr,
+                        Explorer = capCrypto.Explorer,
+                        MarketCapRank = capCrypto.MarketCapRank,
+                    };
 
-                // Check if the corresponding CoinGecko data exists
-                if (geckoDictionary.TryGetValue(capCrypto.Symbol.ToUpper(), out var geckoCrypto))
-                {
-                    
-                    mergedCrypto.IdGecko = geckoCrypto.IdGecko;
-                    mergedCrypto.MarketCap = geckoCrypto.MarketCap;
-                    mergedCrypto.TotalVolume = geckoCrypto.TotalVolume;
-                    mergedCrypto.PriceChange24h = geckoCrypto.PriceChange24h;
-                    mergedCrypto.PriceChangePercentage24h = geckoCrypto.PriceChangePercentage24h;
-                    mergedCrypto.TotalSupply = geckoCrypto.TotalSupply;
-                    mergedCrypto.Image = geckoCrypto.Image;
-                    mergedCrypto.LastUpdated = geckoCrypto.LastUpdated;
+                    // Check if corresponding CoinGecko data exists and merge
+                    var geckoCrypto = geckoData.FirstOrDefault(g =>
+                        g.Symbol.Equals(capCrypto.Symbol, StringComparison.OrdinalIgnoreCase));
+                    if (geckoCrypto != null)
+                    {
+                        mergedCrypto.IdGecko = geckoCrypto.IdGecko;
+                        mergedCrypto.MarketCap = geckoCrypto.MarketCap;
+                        mergedCrypto.TotalVolume = geckoCrypto.TotalVolume;
+                        mergedCrypto.PriceChange24h = geckoCrypto.PriceChange24h;
+                        mergedCrypto.PriceChangePercentage24h = geckoCrypto.PriceChangePercentage24h;
+                        mergedCrypto.TotalSupply = geckoCrypto.TotalSupply;
+                        mergedCrypto.Image = geckoCrypto.Image;
+                        mergedCrypto.LastUpdated = geckoCrypto.LastUpdated;
+                    }
+
+                    mergedData.Add(mergedCrypto);
                 }
-
-                mergedData.Add(mergedCrypto);
             }
 
-            // Additional data Check which don't correspond .
-            /*foreach (var geckoCrypto in geckoData)
+
+            if (geckoData != null && geckoData.Any())
             {
-                if (!capData.Any(c => c.IdGecko == geckoCrypto.IdGecko))
+                foreach (var geckoCrypto in geckoData)
                 {
-                    mergedData.Add(geckoCrypto); 
+                    // Only  it doesn't already exist in mergedData
+                    if (!mergedData.Any(m => m.Symbol.Equals(geckoCrypto.Symbol, StringComparison.OrdinalIgnoreCase)))
+                    {
+                        mergedData.Add(new Cryptocurrency
+                        {
+                            IdGecko = geckoCrypto.IdGecko,
+                            Symbol = geckoCrypto.Symbol,
+                            Name = geckoCrypto.Name,
+                            CirculatingSupply = geckoCrypto.CirculatingSupply,
+                            TotalSupply = geckoCrypto.TotalSupply,
+                            MaxSupply = geckoCrypto.MaxSupply,
+                            MarketCap = geckoCrypto.MarketCap,
+                            TotalVolume = geckoCrypto.TotalVolume,
+                            CurrentPrice = geckoCrypto.CurrentPrice,
+                            PriceChange24h = geckoCrypto.PriceChange24h,
+                            PriceChangePercentage24h = geckoCrypto.PriceChangePercentage24h,
+                            MarketCapRank = geckoCrypto.MarketCapRank,
+                            Image = geckoCrypto.Image,
+                            LastUpdated = geckoCrypto.LastUpdated
+                        });
+                    }
                 }
-            }*/
+
+
+            }
 
             return mergedData;
         }
